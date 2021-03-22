850d1db9ecd100644aa78182a85fbcf6814440a1
josiemoran <79425452+josiemoran@users.noreply.github.com>
2021-03-22 16:33:15 -0500

    Changes

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -3,31 +3,31 @@ query = ""
 results = ""
 
 btnAdd.onclick = function() {
-  query = "INSERT INTO customer VALUES ('17','Jesse Antiques','1113 F St','Omaha','NE','68178')"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
+    query = "INSERT INTO customer VALUES ('17','Jesse Antiques','1113 F St','Omaha','NE','68178')"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
 
-  if (req.status == 200) { 
-    if (req.responseText == 500) { 
-      console.log("You have added a Customer!")
-    } else
-      console.log("Could not add the customer.")
-  } else {
-    console.log("Error: " + req.status);
-  }
+    if (req.status == 200) {
+        if (req.responseText == 500) {
+            console.log("You have added a Customer!")
+        } else
+            console.log("Could not add the customer.")
+    } else {
+        console.log("Error: " + req.status);
+    }
 
-  query = `SELECT name from customer ORDER BY customer_id DESC`
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
+    query = `SELECT name from customer ORDER BY customer_id DESC`
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
 
-  if (req.status == 200) { 
-    results = JSON.parse(req.responseText)
-  } else {
-    console.log(`Error: ${req.status}`);
-  }
-  let customersAdd = ""
-  for (i = 0; i <= results.length - 1; i++)
-    customersAdd = customersAdd + results[i] + "\n"
-  txtaAdd.value = customersAdd
+    if (req.status == 200) {
+        results = JSON.parse(req.responseText)
+    } else {
+        console.log(`Error: ${req.status}`);
+    }
+    let customersAdd = ""
+    for (i = 0; i <= results.length - 1; i++)
+        customersAdd = customersAdd + results[i] + "\n"
+    txtaAdd.value = customersAdd
 }
-btnNext.onclick=function(){
-  ChangeForm(customerUpdate)
+btnNext.onclick = function() {
+    ChangeForm(customerUpdate)
 }
--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -3,64 +3,64 @@ query = ""
 results = ""
 
 customerDelete.onshow = function() {
-  drpDelete.clear()
-  query = "SELECT name from customer"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
-
-  if (req.status == 200) { 
-    customerDeleteR = JSON.parse(req.responseText)
-    console.log(customerDeleteR)
-  }
-  if (customerDeleteR.length == 0) {
-    NSB.MsgBox("There are no customers to delete.")
-  } else {
-    for (i = 0; i <= customerDeleteR.length - 1; i++)
-      drpDelete.addItem(customerDeleteR[i])
-  }
-}
-
-drpDelete.onclick = function(s) {
-  if (typeof(s) == "object")
-    return
-  else {
-    drpDelete.value = s 
-    let DeleteName = s
-    let found = false
-    for (i = 0; i <= customerDeleteR.length - 1; i++) {
-      if (DeleteName == customerDeleteR[i]) {
-        found = true;
-        break;
-      }
-    }
-    if (found == false)
-     txtaDelete.value = `This customer does not exist. ${DeleteName} \n ${customerDeleteR}`
-    else if (found == true) {
-      query = "DELETE FROM customer WHERE name = " + '"' + DeleteName + '"'
-      req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
-      
-      if (req.status == 200) {
-        if (req.responseText == 500) 
-          console.log(`You successfully deleted the customer: ${DeleteName}`)
-        else
-          console.log(`There was a problem deleting ${DeleteName}.`)
-      } else {
-        console.log(`Error: ${req.status}`);
-      }
-    }
-    query = `SELECT name from customer`
+    drpDelete.clear()
+    query = "SELECT name from customer"
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
 
     if (req.status == 200) {
-      customerAfterDelete = JSON.parse(req.responseText)
-    } else {
-      console.log(`Error: ${req.status}`);
+        customerDeleteR = JSON.parse(req.responseText)
+        console.log(customerDeleteR)
     }
-    let customersLeft = ""
-    for (i = 0; i <= customerAfterDelete.length - 1; i++)
-      customersLeft = customersLeft + customerAfterDelete[i] + "\n"
-    txtaDelete.value = customersLeft
-  }
+    if (customerDeleteR.length == 0) {
+        NSB.MsgBox("There are no customers to delete.")
+    } else {
+        for (i = 0; i <= customerDeleteR.length - 1; i++)
+            drpDelete.addItem(customerDeleteR[i])
+    }
 }
-btnDeleteNxt.onclick=function(){
-  ChangeForm(customerAdd)
+
+drpDelete.onclick = function(s) {
+    if (typeof(s) == "object")
+        return
+    else {
+        drpDelete.value = s
+        let DeleteName = s
+        let found = false
+        for (i = 0; i <= customerDeleteR.length - 1; i++) {
+            if (DeleteName == customerDeleteR[i]) {
+                found = true;
+                break;
+            }
+        }
+        if (found == false)
+            txtaDelete.value = `This customer does not exist. ${DeleteName} \n ${customerDeleteR}`
+        else if (found == true) {
+            query = "DELETE FROM customer WHERE name = " + '"' + DeleteName + '"'
+            req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
+
+            if (req.status == 200) {
+                if (req.responseText == 500)
+                    console.log(`You successfully deleted the customer: ${DeleteName}`)
+                else
+                    console.log(`There was a problem deleting ${DeleteName}.`)
+            } else {
+                console.log(`Error: ${req.status}`);
+            }
+        }
+        query = `SELECT name from customer`
+        req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mjm46639&pass=" + pw + "&database=mjm46639&query=" + query)
+
+        if (req.status == 200) {
+            customerAfterDelete = JSON.parse(req.responseText)
+        } else {
+            console.log(`Error: ${req.status}`);
+        }
+        let customersLeft = ""
+        for (i = 0; i <= customerAfterDelete.length - 1; i++)
+            customersLeft = customersLeft + customerAfterDelete[i] + "\n"
+        txtaDelete.value = customersLeft
+    }
+}
+btnDeleteNxt.onclick = function() {
+    ChangeForm(customerAdd)
 }
